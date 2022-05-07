082c3dec675ad159e77ccc8a20a89338019c2a25
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2022-05-07 13:47:05 -0400

    cors headers

--- lib/github/manager.go
+++ lib/github/manager.go
@@ -29,6 +29,9 @@ func (m *Manager) Get(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	data, _ := json.Marshal(m.Cache)
 	w.Write(data)
 }
